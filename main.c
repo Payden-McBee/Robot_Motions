8e6347a0a40f95886c8568d1143d31d2fadca9ca
Payden McBee <c15payden.mcbee@usafa.edu>
2013-11-20 21:03:28 -0700

    This code makes the robot spin in circles. I had to manipulate what mode each signal was set to. Mod7 is reset/set nad Mod5 is reset. I switch which signal, TA0CCR1, R0 get the signal to determine which one gets ground and which gets power, thus turning whichever way I choose.

--- main.c
+++ main.c
@@ -24,34 +24,35 @@ void main(void) {
 
 	TA1CTL &= ~MC1|MC0;            // stop timer A0
 
-		TA1CTL |= TACLR;                // clear timer A0
+	TA1CTL |= TACLR;                // clear timer A0
 
-		TA1CTL |= TASSEL1;           // configure for SMCLK
+	TA1CTL |= TASSEL1;           // configure for SMCLK
 
 
 	TA0CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
 	TA0CCTL0 |= OUTMOD_7;		 // set TACCTL0 to Reset
 
 	TA0CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
-	TA0CCR1 = 25;//50;                // set duty cycle to 50/100 (50%)
+	TA0CCR1 = 50;                // set duty cycle to 50/100 (50%)
 
+	TA1CCTL1 |= OUTMOD_7;
+	//TA1CCTL0 |= OUTMOD_7;
 
-	TA1CCTL0 |= OUTMOD_7;
-	TA1CCTL1 |= OUTMOD_5;
 
 	TA1CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
-	TA1CCR1 = 50;//50;                // set duty cycle to 50/100 (50%)
+	TA1CCR1 = 50;                // set duty cycle to 50/100 (50%)
 
 	TA0CTL |= MC0;                // count up
 
 	TA1CTL |= MC0;
 
 	while (1) {
-			//__delay_cycles(1000000);
-	 	 //	TA0CCR1 = 50;            // set duty cycle to 50/100 (50%)
-	 	 	//TA0CCR0 = 100;
+
+
+
+			//TA0CCR0 = 100;
 	    	//__delay_cycles(1000000);
-	     //   TACCR1 = 0;            // set duty cycle to 75/100 (75%)
+	       // TA1CCR1 = 0;            // set duty cycle to 75/100 (75%)
 	        /*__delay_cycles(1000000);
 	        TACCR1 = 100;            // set duty cycle to 100/100 (100%)
 	        __delay_cycles(1000000);
