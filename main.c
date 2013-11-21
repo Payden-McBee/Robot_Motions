4c77e9736ac54c7b2982e3ee17b9539d3d747839
Payden McBee <c15payden.mcbee@usafa.edu>
2013-11-20 21:51:53 -0700

    I wrote a library for robotMotion where I have commands to move forward, backward, turn left and right. I also had to clear the modes whenever I wanted to change them. This is because bit set does not clear any previous bit, so once I went into reset/set mode, 111, I could never get out of it until I cleared the bits. I did this by &= ~ every mode every time I want to change the modes.

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <msp430.h> 
-
+#include "robotMotion.h"
 /*
  * main.c
  */
@@ -29,14 +29,11 @@ void main(void) {
 	TA1CTL |= TASSEL1;           // configure for SMCLK
 
 
-	TA0CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-	TA0CCTL0 |= OUTMOD_7;		 // set TACCTL0 to Reset
 
 	TA0CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
 	TA0CCR1 = 50;                // set duty cycle to 50/100 (50%)
 
-	TA1CCTL1 |= OUTMOD_7;
-	//TA1CCTL0 |= OUTMOD_7;
+
 
 
 	TA1CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
@@ -45,10 +42,26 @@ void main(void) {
 	TA0CTL |= MC0;                // count up
 
 	TA1CTL |= MC0;
+	//go the other way code
+		/*TA0CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+		TA0CCTL0 |= OUTMOD_7;		 // set TACCTL0 to Reset
 
-	while (1) {
+		TA1CCTL1 |= OUTMOD_7;*/
 
+	moveForward();
+	//while (1) {
+	//	__delay_cycles(1000000);
+	//	TA0CCTL1 &= ~OUTMOD_7;        // clear mode
+		//	TA0CCTL0 &= ~OUTMOD_7;		 // clear mode
+//
+	//		TA1CCTL1 &= ~OUTMOD_7;  //clear mode
+		/*	TA1CCTL0 &= ~OUTMOD_7;  //clear mode
 
+		TA1CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+			TA1CCTL0 |= OUTMOD_7;		 // set TACCTL0 to Reset
+
+			TA0CCTL1 |= OUTMOD_7;*/
+			//TA1CCTL0 |= OUTMOD_7;
 
 			//TA0CCR0 = 100;
 	    	//__delay_cycles(1000000);
@@ -57,6 +70,6 @@ void main(void) {
 	        TACCR1 = 100;            // set duty cycle to 100/100 (100%)
 	        __delay_cycles(1000000);
 	        TACCR1 = 25;            // set duty cycle to 25/100 (25%)*/
-	    }
+	   // }
 	
 }
