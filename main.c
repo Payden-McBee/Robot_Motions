5f43c4682c425fe4006d15e9e7a115a3dcc2f5fa
Payden McBee <c15payden.mcbee@usafa.edu>
2013-11-20 20:13:21 -0700

    I got both wheels to go forward. The problem was that I tried to reset on Ta0CCR0, which you can't do

--- main.c
+++ main.c
@@ -5,12 +5,15 @@
  */
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	P1DIR|=BIT3; //TA0.0
-	P1DIR|=BIT4; //TA0.1
-	P1DIR|=BIT8; //TA1.0
-	P1DIR|=BIT9; //TA1.1
+	P1DIR|=BIT1; //TA0.0
+	P1DIR|=BIT2; //TA0.1
+	P2DIR|=BIT0; //TA1.0
+	P2DIR|=BIT1; //TA1.1
 
-	//P1SEL|=BIT1;
+	P1SEL|=BIT1;
+	P1SEL|=BIT2;
+	P2SEL|=BIT0;
+	P2SEL|=BIT1;
 
 
 	TA0CTL &= ~MC1|MC0;            // stop timer A0
@@ -19,21 +22,37 @@ void main(void) {
 
 	TA0CTL |= TASSEL1;           // configure for SMCLK
 
-	TA0CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
-	TA0CCR1 = 50;                // set duty cycle to 50/100 (50%)
+	TA1CTL &= ~MC1|MC0;            // stop timer A0
 
-	TA0CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-	TA0CCTL0 |= OUTMOD_5;		 // set TACCTL0 to Reset
+		TA1CTL |= TACLR;                // clear timer A0
+
+		TA1CTL |= TASSEL1;           // configure for SMCLK
+
+
+	TA0CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+	TA0CCTL0 |= OUTMOD_7;		 // set TACCTL0 to Reset
+
+	TA0CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+	TA0CCR1 = 25;//50;                // set duty cycle to 50/100 (50%)
+
+
+	TA1CCTL0 |= OUTMOD_7;
+	TA1CCTL1 |= OUTMOD_5;
+
+	TA1CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+	TA1CCR1 = 50;//50;                // set duty cycle to 50/100 (50%)
 
 	TA0CTL |= MC0;                // count up
 
+	TA1CTL |= MC0;
+
 	while (1) {
-			__delay_cycles(1000000);
-	 	 	TA0CCR1 = 50;            // set duty cycle to 50/100 (50%)
-	 	 	TA0CCR0 = 100;
-	    	/*__delay_cycles(1000000);
-	        TACCR1 = 75;            // set duty cycle to 75/100 (75%)
-	        __delay_cycles(1000000);
+			//__delay_cycles(1000000);
+	 	 //	TA0CCR1 = 50;            // set duty cycle to 50/100 (50%)
+	 	 	//TA0CCR0 = 100;
+	    	//__delay_cycles(1000000);
+	     //   TACCR1 = 0;            // set duty cycle to 75/100 (75%)
+	        /*__delay_cycles(1000000);
 	        TACCR1 = 100;            // set duty cycle to 100/100 (100%)
 	        __delay_cycles(1000000);
 	        TACCR1 = 25;            // set duty cycle to 25/100 (25%)*/
